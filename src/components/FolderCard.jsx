ab3828e2133348f3eff99d9cbf1bd08b1674f290
Legimmort82 <legendlegendery1212@gmail.com>
2024-09-10 15:11:43 +0330

    animated folder open icon

--- src/components/FolderCard.jsx
+++ src/components/FolderCard.jsx
@@ -1,12 +1,21 @@
-import Image from "next/image"
+import Image from "next/image";
+import { useState } from "react";
 
-function FolderCard({folderName, img}) {
+function FolderCard({ folderName, img }) {
+  const [open, setOpen] = useState(false);
   return (
     <div className="bg-Primary-600 flex justify-between items-center w-[30%] rounded-md px-10 py-3 mb-4 border-2 border-Primary-400">
-        <p className="text-[30px] text-white font-semibold">{folderName}</p>
-        <Image className="w-6 h-6 cursor-pointer" src={img} alt=""/>
+      <p className="text-[30px] text-white font-semibold">{folderName}</p>
+      <Image
+        className={`w-6 h-6 cursor-pointer transition-all duration-300 ease-in-out ${
+          open ? "rotate-180" : ""
+        }`}
+        src={img}
+        alt=""
+        onClick={() => setOpen(!open)}
+      />
     </div>
-  )
+  );
 }
 
-export default FolderCard
+export default FolderCard;
