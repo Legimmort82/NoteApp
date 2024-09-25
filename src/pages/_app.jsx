91c4133fb901cd494436cdc12c7227806ed7a971
Legimmort82 <legendlegendery1212@gmail.com>
2024-09-25 11:43:42 +0330

    fixed

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "@/assets/styles/globals.css";
 import { DarkModeProvider } from "@/context/DarkModeContext";
 import { Nunito } from "next/font/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
